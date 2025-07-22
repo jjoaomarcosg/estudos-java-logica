c0fcaafe2281bb7f042ebf8ead69187c38ad9c6c
João Marcos <jmsgdd297@gmail.com>
2025-07-22 20:55:14 -0300

    atualiza git.java com anotações

--- src/notas/git.java
+++ src/notas/git.java
@@ -1,4 +1,26 @@
 package notas;
 
 public class git {
+    public static void main(String[] args) {
+
+
+        /*
+        Git introdução de comandos
+
+        git init                    # Iniciar repositório
+
+        git status                 # Ver estado dos arquivos
+
+        git add .                  # Adicionar todos os arquivos
+
+        git commit -m "mensagem"   # Salvar os arquivos com mensagem
+
+        git push                   # Enviar para o GitHub
+
+        git pull                   # Baixar atualizações do GitHub
+
+         */
+
+    }
+
 }
